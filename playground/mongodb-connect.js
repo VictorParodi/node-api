da193469d4e9b118d538b26a9eae0c57a5403f44
Victor <victor.parodi.montero@gmail.com>
2018-01-08 21:08:13 -0500

    rama1 commit 1
    Creando la colección Users e insertando el primer documento.

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -7,5 +7,18 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, db) => {
 
     console.log('Connected to MongoDB server');
 
+    db.collection('Users').insertOne({
+        name: 'Ana',
+        age: 21,
+        location: 'Germany'
+    }, (error, result) => {
+        if (error) {
+            return console.log('Unable to insert the document');
+        }
+
+        console.log('Document inserted');
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });
+
     db.close();
 });
