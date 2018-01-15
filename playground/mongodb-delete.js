ba421c4f8b83b57d787a6de730c998b811b7ee7c
Victor <victor.parodi.montero@gmail.com>
2018-01-14 21:06:35 -0500

    rama3 commit 4
    Eliminando dcumentos de la colección Todo.

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,45 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, db) => {
+    if (error) {
+        return console.log('Connect to Data Base faild');
+    }
+
+    // findOneAndDelete()
+    db.collection('Todo')
+        .findOneAndDelete({
+            text: 'Walk the cat'
+        })
+        .then((result) => {
+            console.log(result);
+        })
+        .catch((error) => {
+            console.log('Delete action faild', error);
+        });
+
+    // deleteMany()
+    // db.collection('Todo')
+    //     .deleteMany({
+    //         text: "Walk the dog"
+    //     })
+    //     .then((result) => {
+    //         console.log(result);
+    //     })
+    //     .catch((error) => {
+    //         console.log('Delete action faild');
+    //     });
+
+    // deleteOne()
+    // db.collection('Todo')
+    //     .deleteOne({
+    //         text: 'Walk the dog'
+    //     })
+    //     .then((result) => {
+    //         console.log(result);
+    //     })
+    //     .catch((error) => {
+    //         console.log('Delecte action faild', error);
+    //     });
+
+    console.log('Connected to Data Base');
+});
